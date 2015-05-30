fe98a7ab3dde2ecb8081ff6042cfb8c423211fb1
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2015-05-30 00:57:57 -0400

    Initial commit
    Proof-of-concept work. See Schematik.Number() class.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Schematik
+The proof-of-concept human-friendly JSON schema generator. Heavily inspired by
+[Chai](http://chaijs.com/), [Squel.js](https://hiddentao.github.io/squel/)
+and [lodash](https://lodash.com/).
+
+The goal of the project is not to implement every bit and quirk or the IETF
+specification, but to provide a bit more readable syntax for writing JSON
+schemas.
+
+## Example
+Instead of writing this:
+```json
+{
+  "type": "number",
+  "required": true,
+  "minimum": -180,
+  "maximum": 180,
+  "exclusiveMinimum": true,
+  "exclusiveMaximum": true,
+  "multipleOf": 45
+}
+```
+write this:
+```js
+Schematik.number().in.exclusive.range(-180, 180).that.is.a.multiple.of(45)
+```
---
+++ main.js
@@ -0,0 +1,31 @@
+// ------------------------------------------------------------------------- //
+//                                                                           //
+// Schematik - human friendly JSON schema builder.                           //
+//                                                                           //
+// ------------------------------------------------------------------------- //
+_         = require('lodash');
+Schematik = require('./lib/types/schematik');
+
+require('./lib/types/number');
+require('./lib/types/boolean');
+require('./lib/types/null');
+
+// Shortcut for creating a Schematik.Boolean
+Schematik.prototype.boolean = function () {
+  return new Schematik.Boolean(this.self());
+};
+
+// Shortcut for creating a Schematik.Number
+Schematik.prototype.number = function () {
+  return new Schematik.Number(this.self());
+};
+
+// Shortcut for creating a Schematik.Integer
+Schematik.prototype.integer = function () {
+  return new Schematik.Integer(this.self());
+};
+
+_.assign(Schematik, Schematik.prototype);
+
+
+module.exports = Schematik;
