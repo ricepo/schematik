7b6cc922d0ab199987c30bb7f71ce133ee0089f5
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2015-07-18 00:19:55 -0400

    Array finished

--- src/types/array.js
+++ src/types/array.js
@@ -14,7 +14,7 @@ import Additional   from '../flags/additional';
 /**
  * Schematik.Array
  *
- * @classdesc Schematik array type representation.
+ * @classdesc       Schematik array type representation.
  */
 class SkArray extends Schematik {
 
@@ -25,28 +25,76 @@ class SkArray extends Schematik {
 
 }
 
+
 /**
  * # .items()
  *
- * @desc          Specifies array item schemas.
- * @returns       A copy of Schematik with item schemas set.
+ * @desc            Specifies array item schemas.
+ * @returns         A copy of Schematik with item schemas set.
  */
 function items(...values) {
-  if (arguments.length === 0) {
-    throw new Error('Schematik.Array.items: need at least one argument.');
+
+  // If we have the `additional` flag, modify `additionalItems` instead
+  if (this.flag('additional')) {
+    let value = values[0] === undefined ? true : values[0];
+    if (this.flag('negate')) { value = false; }
+
+    if (typeof value !== 'object' && typeof value !== 'boolean') {
+      throw new Error('Schematik.Array.items: value must be object or bool.');
+    }
+
+    let result = this.clone();
+    result[schema] = this[schema].merge({ additionalItems: value });
+    return result;
   }
 
-  // Map all items to their final object forms
-  values = values.map(i => (i instanceof Schematik) ? i.done() : i);
+  // Otherwise, modify `items`
+  else {
+    if (arguments.length === 0) {
+      throw new Error('Schematik.Array.items: need at least one argument.');
+    }
 
-  // If current schema.items is a single object, wrap it into array
-  let current = this[schema].items;
-  current = current ? values.concat(current) : values;
-  current = current.length === 1 ? current[0] : current;
+    // Map all items to their final object forms
+    values = values.map(i => (i instanceof Schematik) ? i.done() : i);
 
-  let result  = this.clone();
-  result[schema] = this[schema].merge({ items: current });
+    // If current schema.items is a single object, wrap it into array
+    let current = this[schema].items;
+    current = current ? values.concat(current)  : values;
+    current = current.length === 1 ? current[0] : current;
 
+    let result  = this.clone();
+    result[schema] = this[schema].merge({ items: current });
+
+    return result;
+  }
+
+}
+
+
+/**
+ * # .length()
+ *
+ * @desc            Specifies array length.
+ * @returns         A copy of Schematik with array lengths set.
+ */
+function length(a, b) {
+  if (typeof a !== 'number') {
+    throw new Error('Schematik.Array.length: length must be a number.');
+  }
+
+  let diff = { };
+  if      (this.flag('range') === 'max') { diff.maxItems = a; }
+  else if (this.flag('range') === 'min') { diff.minItems = a; }
+  else if (typeof b === 'number') {
+    diff.minItems = a;
+    diff.maxItems = b;
+  }
+  else {
+    diff.minItems = diff.maxItems = a;
+  }
+
+  let result = this.clone();
+  result[schema] = this[schema].merge(diff);
   return result;
 }
 
@@ -67,6 +115,7 @@ export default function(Schematik, Util) {
   Schematik.array = Schematik.prototype.array = function() {
     let result = new Schematik.Array();
     this.self().copyTo(result);
+    result.__type('array');
     return result;
   };
 
@@ -79,5 +128,8 @@ export default function(Schematik, Util) {
   /**
    * Attach array-specific methods.
    */
-  Util.addChainable(Schematik.Array.prototype, 'items', items);
+  Util.addChainable(Schematik.Array.prototype, 'items',  items);
+  Util.addChainable(Schematik.Array.prototype, 'len',    length);
+  Util.addChainable(Schematik.Array.prototype, 'count',  length);
+  Util.addChainable(Schematik.Array.prototype, 'length', length);
 }
