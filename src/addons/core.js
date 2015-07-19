489d1a5737108c6d3e1990cc253024575e2c192b
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2015-07-19 17:40:27 -0400

    Wrapped up enum, one, any, all and not

--- src/addons/core.js
+++ src/addons/core.js
@@ -5,6 +5,7 @@
  * @license         MIT
  */
 
+import Schematik        from '../schematik';
 import { conjunctions } from '../config';
 
 /**
@@ -50,20 +51,117 @@ export function of(...args) {
 
 
 /**
+ * .whitelist()
+ *
+ * @desc            Specifies a set of acceptable values.
+ */
+export function whitelist(...args) {
+  if (args.length === 0) {
+    throw new Error('Must have at least one argument.');
+  }
+
+  let result = new Schematik();
+  let diff   = { enum: args };
+
+  return result.schema(diff);
+}
+
+
+/**
+ * .one()
+ *
+ * @desc            Specifies a match against exactly one of schemas.
+ */
+export function one(...args) {
+  if (args.length === 0) {
+    throw new Error('Must have at least one argument.');
+  }
+
+  let result = new Schematik();
+  let diff   = {
+    oneOf: args.map((i) => (i instanceof Schematik) ? i.done() : i)
+  };
+
+  return result.schema(diff);
+}
+
+
+/**
+ * .all()
+ *
+ * @desc            Specifies a match against all of the schemas.
+ */
+export function all(...args) {
+  if (args.length === 0) {
+    throw new Error('Must have at least one argument.');
+  }
+
+  let result = new Schematik();
+  let diff   = {
+    allOf: args.map((i) => (i instanceof Schematik) ? i.done() : i)
+  };
+
+  return result.schema(diff);
+}
+
+
+/**
+ * .any()
+ *
+ * @desc            Specifies a match against at least one of schemas.
+ */
+export function any(...args) {
+  if (args.length === 0) {
+    throw new Error('Must have at least one argument.');
+  }
+
+  let result = new Schematik();
+  let diff   = {
+    anyOf: args.map((i) => (i instanceof Schematik) ? i.done() : i)
+  };
+
+  return result.schema(diff);
+}
+
+
+/**
+ * .not()
+ *
+ * @desc            Negates the match against a schema.
+ */
+export function not(schema) {
+  if (typeof schema !== object) {
+    throw new Error('Schema must be an object.');
+  }
+
+  let result = new Schematik();
+  let diff   = { not: schema instanceof Schematik ? schema.done() : schema };
+
+  return result.schema(diff);
+}
+
+
+/**
  * Export a middleware function.
  */
 export default function core(Schematik, Util) {
 
-  Util.addProperty(Schematik,           'required', required);
-  Util.addProperty(Schematik.prototype, 'required', required);
+  Util.addProperty(Schematik,            'required', required);
+  Util.addProperty(Schematik.prototype,  'required', required);
 
-  Util.addProperty(Schematik,           'optional', optional);
-  Util.addProperty(Schematik.prototype, 'optional', optional);
+  Util.addProperty(Schematik,            'optional', optional);
+  Util.addProperty(Schematik.prototype,  'optional', optional);
 
-  Util.addProperty(Schematik.prototype, 'not', negate);
-  Util.addProperty(Schematik.prototype, 'no',  negate);
+  Util.addProperty(Schematik.prototype,  'not',      negate);
+  Util.addProperty(Schematik.prototype,  'no',       negate);
 
-  Util.addChainable(Schematik.prototype, 'of', of);
+  Util.addChainable(Schematik.prototype, 'of',       of);
+  Util.addChainable(Schematik.prototype, 'enum',     whitelist);
+
+  Schematik.one = Schematik.oneOf = one;
+  Schematik.all = Schematik.allOf = all;
+  Schematik.any = Schematik.anyOf = any;
+  Schematik.not = not;
 
   for (let item of conjunctions) {
     Util.addProperty(Schematik.prototype, item);
